fcd137f27fa218e9cdac5331d3d66d43ecd91f38
mattiaise <mattia.isetta@gmail.com>
2025-01-30 15:36:52 +0100

    add commands for time amount ad time slot

--- src/booking_bot.py
+++ src/booking_bot.py
@@ -1,12 +1,56 @@
-from selenium import webdriver 
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime, timedelta
 import data
-import time
 
-class Booking_bot(webdriver.Chrome):
-    def __init__(self):
-       super()
+def login(driver):
+    login = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "//button[@data-cypress='navBar-Accedi']"))
+    )
+    login.click()
+
+    username = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, "username"))
+    )
+    username.send_keys(data.USER)
+    password = driver.find_element(By.ID, "password")
+    password.send_keys(data.PSW)
+
+    send_data = driver.find_element(By.XPATH, "//input[@type='submit']")
+    send_data.click()
+    
+def time_amount(driver, amount):
+    durata = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "durata"))
+    )
+    durata.click()
+
+    time_amount = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, f"//option[@value='{amount*3600}']"))
+    )
+    time_amount.click()
+    durata.click()
+    
+def time_slot(driver, start, amount):
+    slot_start = datetime.strptime(start, "%H:%M")
+    slot_end = slot_start + timedelta(hours=int(amount))
+
+    slot_end_str = slot_end.strftime("%H:%M")
+    slot_start_str = slot_start.strftime("%H:%M")
+
+    time_slot = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, f"//div[@aria-label='time select']//div[starts-with(@aria-label, '{slot_start_str}')]//span[text()=' {slot_start_str}  - {slot_end_str}']"))
+    )
+    time_slot.click()
+    
+def confirm(driver):
+    data_confirm = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[@href='/portalePlanning/biblio/prenota/Riepilogo']"))
+    )
+    data_confirm.click()
+
+    booking_confirm = WebDriverWait(driver, 30).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'btn-primary') and contains(@class, 'btn-lg')]"))
+    )
+    booking_confirm.click()
--- src/main.py
+++ src/main.py
@@ -4,8 +4,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import booking_bot as bb
 import data
 import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--time-amount", required=True)
+parser.add_argument("--start-from", required=True)
+parser.add_argument("--scheduled", required=True)
+args = parser.parse_args()
 
 service = Service(executable_path=data.DRIVER_PATH)
 driver = webdriver.Chrome(service=service)
@@ -13,48 +21,23 @@ driver = webdriver.Chrome(service=service)
 driver.get(data.URL)
 driver.fullscreen_window()
 
-login = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//button[@data-cypress='navBar-Accedi']"))
-)
-login.click()
+bb.login(driver)
 
-username = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.ID, "username"))
-)
-username.send_keys(data.USER)
-password = driver.find_element(By.ID, "password")
-password.send_keys(data.PSW)
-
-send_data = driver.find_element(By.XPATH, "//input[@type='submit']")
-send_data.click()
-
+# scegli fast booking
 fast_booking = WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.ID, "riprenota_title"))
 )
 fast_booking.click()
 
-durata = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.ID, "durata"))
-)
-durata.click()
+bb.time_amount(driver, int(args.time_amount))
 
-time_amount = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//option[@value='3600']"))
-)
-time_amount.click()
-durata.click()
-
+# day picker 
 giorno = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, "//div[@class='days']//div[@aria-description='selezionabile']"))
 )
 giorno.click()
 
-time_slot = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//div[@aria-label='time select']//span[.='22:00 - 23:00']"))
-)
-time_slot.click()
-
+bb.time_slot(driver, args.start_from, args.time_amount)
+bb.confirm(driver)
 
 time.sleep(10)
-
-
