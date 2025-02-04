65c7d0eddc5085f53023ac8382574d392d8f9fca
mattia.isetta <mattia.isetta@studenti.unimi.it>
2025-02-04 10:46:21 +0100

    update timers

--- src/booking_bot.py
+++ src/booking_bot.py
@@ -6,12 +6,12 @@ import data
 import time
 
 def login(driver):
-    login = WebDriverWait(driver, 5).until(
+    login = WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.XPATH, "//button[@data-cypress='navBar-Accedi']"))
     )
     login.click()
 
-    username = WebDriverWait(driver, 5).until(
+    username = WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.ID, "username"))
     )
     username.send_keys(data.USER)
@@ -22,12 +22,12 @@ def login(driver):
     send_data.click()
     
 def time_amount(driver, amount):
-    durata = WebDriverWait(driver, 10).until(
+    durata = WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.ID, "durata"))
     )
     durata.click()
 
-    time_amount = WebDriverWait(driver, 10).until(
+    time_amount = WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.XPATH, f"//option[@value='{amount*3600}']"))
     )
     time_amount.click()
@@ -40,7 +40,7 @@ def time_slot(driver, start, amount):
     slot_end_str = slot_end.strftime("%H:%M")
     slot_start_str = slot_start.strftime("%H:%M")
 
-    time_slot = WebDriverWait(driver, 10).until(
+    time_slot = WebDriverWait(driver, 30).until(
         EC.element_to_be_clickable((By.XPATH, f"//div[@aria-label='time select']//div[starts-with(@aria-label, '{slot_start_str}')]//span[text()=' {slot_start_str}  - {slot_end_str}']"))
     )
     driver.execute_script("arguments[0].scrollIntoView(true);", time_slot)
@@ -48,14 +48,14 @@ def time_slot(driver, start, amount):
     time_slot.click()
     
 def confirm(driver):
-    data_confirm = WebDriverWait(driver, 10).until(
+    data_confirm = WebDriverWait(driver, 30).until(
         EC.element_to_be_clickable((By.XPATH, "//a[@href='/portalePlanning/biblio/prenota/Riepilogo']"))
     )
     driver.execute_script("arguments[0].scrollIntoView(true);", data_confirm)
     time.sleep(2)
     data_confirm.click()
     
-    booking_confirm = WebDriverWait(driver, 10).until(
+    booking_confirm = WebDriverWait(driver, 15).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.my-2"))
     )
     time.sleep(2)
